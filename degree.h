c2044fb37256059ae79b5ed0ad9af7167f043a7c
OrionBT <64709219+OrionBT@users.noreply.github.com>
2020-05-30 15:20:54 -0700

    Create degree.h

---
+++ degree.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+enum DegreeProgram
+{UNDECIDED = 0, SECURITY = 1, NETWORK = 2, SOFTWARE = 3 }; 
+
+
+static const string degreeProgramStrings[4] = {"UNDECIDED","SECURITY","NETWORK","SOFTWARE" };
+#pragma once
